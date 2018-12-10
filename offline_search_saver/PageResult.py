63c0d448bc26f67963057ca979dfdb4fce9c12af
Paul Tudan <ptudan@udel.edu>
2018-12-10 17:20:54 -0500

    mostly working now

--- offline_search_saver/PageResult.py
+++ offline_search_saver/PageResult.py
@@ -10,3 +10,11 @@ class PageResult:
 
 	def add_result_to_internal(self, new_page_result):
 		self.internal_links.append(new_page_result)
+
+	def diag_print(self):
+		print("Title: " + str(self.name)) # The title of the link
+		print("URL: " + str(self.link)) # The external link
+		print("Description: " + str(self.description)) # The description of the link
+		print("Page: " + str(self.page)) # What page this result was on (When searching more than one page)
+		print("Index: " + str(self.index)) # What index on this page it was on
+		print("Internal links: " + str(internal_links)) # all links contained on the page
--- offline_search_saver/main.py
+++ offline_search_saver/main.py
@@ -1,43 +1,54 @@
 import os
-#os.chdir("C:\\Users\\Paul\\Documents\\Senior\\personal projects\\offline_search_saver\\g")
-from google import google
 import sys
+import io
 import time
-from urllib.request import urlopen
+import re
 from bs4 import BeautifulSoup
+from google.google import google
 from PageResult import PageResult
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from urllib.error import HTTPError
 results_path = ''
 
-print('Enter your search:')
+
+print('Enter your search: ', end='')
 q = input()
-print('Enter num pages desired:')
+print('Enter num pages desired: ', end='')
 p = (int)(input())
-print('Enter the save depth for links (2 recommended):')
+print('Enter the save depth for links (2 recommended): ', end='')
 d = (int)(input())
 
 initial_results = google.search(q, p)
-
+fs = ''
+for c in q:
+	if c not in ['*','.','/','\\','[',']',':',';','|','=','+',',',"'",'"']:
+		fs += c
 os.chdir('results')
-folder_name = q+'_'+str(time.time())
+folder_name = str(time.time())+'_'+fs
 os.mkdir(folder_name)
 os.chdir(folder_name)
-i = initial_results[0]
-# print(i.name) # The title of the link
-# print(i.link) # The external link
-# print(i.google_link) # The google link
-# print(i.description) # The description of the link
-# print(i.thumb) # The link to a thumbnail of the website (NOT implemented yet)
-# print(i.cached) # A link to the cached version of the page
-# print(i.page) # What page this result was on (When searching more than one page)
-# print(i.index) # What index on this page it was on
-# print(i.number_of_results) # The total number of results the query returned
 
-# final_results = []
-# for i in initial_results:
-p = PageResult(i.name, i.link, i.page, i.index, i.description)
-h = urlopen(p.link).read()
-filename = 'result' + str(p.page) + '_' + str(p.index) + '.html'
-f = open(filename, 'w')
-s = BeautifulSoup(h, 'html.parser', from_encoding='utf-8')
-f.write(str(s.prettify()))
-f.close()
+
+
+final_results = []
+for i in initial_results:
+	p = PageResult(i.name, i.link, i.page, i.index, i.description)
+	try:
+		h = urlopen(p.link).read()
+		d = urlparse(p.link)
+		filename = 'pg' + str(p.page) + '_ind' + str(p.index)
+		if(d is not None):
+			filename += '_' + d.netloc
+			# dom = re.search(r'(w{3}\.)([A-Za-z0-9]*)', d.netloc)
+			# print(d.netloc)
+			# if(dom is not None):
+			# 	print(dom.group(2))
+		filename+='.html'
+		with io.open(filename, 'w', encoding='utf-8') as f:
+			f.write(str(BeautifulSoup(h, 'html.parser', from_encoding='utf-8')))
+		#f.write(str(s.prettify()))
+		f.close()
+	except HTTPError as err:
+		print(str(err) + ' From: ' + p.name)
+		continue
